26375e06444d4d185bfe9e08e21b949173746f34
Victor Toscano <victor.ijsn@gmail.com>
2024-01-03 19:16:14 +0000

    mensagem modificada para o paraense do setor

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Andrea, sei que está com saudade de ir no barzinho com os amigos e passar várias vergonhas, né, minha filha? 🤩</h1>
+    return <><h1>Edil, essa é pra você! 🤩</h1>
+    <iframe src="https://giphy.com/embed/J6DibUmYij1CfP0yyv" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/POPline-joelma-popline-poplinemasks4all-J6DibUmYij1CfP0yyv">via GIPHY</a></p>
+    <p> Esse é o link para  
+  <a href="https://lebillet.com.br/event/1452/isso-e-calypso-festival-13-abril-Cariacica-ES"> o show da Joelma no Espírito Santo!</a>.
+</p></>
 }
 
 export default Home;
