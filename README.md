32ab6d29c5989cb1f88e4ea86a8470a15df8c255
Victor Toscano <victor.ijsn@gmail.com>
2024-01-09 14:00:39 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do site https://www.tabnews.com.br para o curso.dev do Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
 function Home() {
-    return <><h1>Edil, essa é pra você! 🤩</h1>
-    <iframe src="https://giphy.com/embed/J6DibUmYij1CfP0yyv" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/POPline-joelma-popline-poplinemasks4all-J6DibUmYij1CfP0yyv">via GIPHY</a></p>
-    <p> Esse é o link para  
-  <a href="https://lebillet.com.br/event/1452/isso-e-calypso-festival-13-abril-Cariacica-ES"> o show da Joelma no Espírito Santo!</a>.
-</p></>
+  return (
+    <>
+      <h1>Edil, essa é pra você! 🤩</h1>
+      <iframe
+        src="https://giphy.com/embed/J6DibUmYij1CfP0yyv"
+        width="480"
+        height="270"
+        frameBorder="0"
+        class="giphy-embed"
+        allowFullScreen
+      ></iframe>
+      <p>
+        <a href="https://giphy.com/gifs/POPline-joelma-popline-poplinemasks4all-J6DibUmYij1CfP0yyv">
+          via GIPHY
+        </a>
+      </p>
+      <p>
+        {" "}
+        Esse é o link para
+        <a href="https://lebillet.com.br/event/1452/isso-e-calypso-festival-13-abril-Cariacica-ES">
+          {" "}
+          o show da Joelma no Espírito Santo!
+        </a>
+        .
+      </p>
+    </>
+  );
 }
 
 export default Home;
